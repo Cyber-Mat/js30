c67a0ee0b364676f5a50ea6a0ea1a065c8bb0fdb
cyber-mat <olaniyanolamide@gmail.com>
2020-05-26 15:27:50 +0100

    Works, but not ready

---
+++ 01 - JS Drum kit/app.js
@@ -0,0 +1,20 @@
+function play(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    //console.log(e.keyCode);
+    if (!audio) return
+    audio.currentTime = 0;
+    audio.play();
+    document.querySelector(`.key[data-key="${e.keyCode}"]`).classList.add('playing');
+}
+
+
+
+window.addEventListener('keypress', play);
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
